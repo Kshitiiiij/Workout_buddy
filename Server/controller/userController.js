b42f451d7889bda38d3e49993a84c86d32b03a69
kshitij <kshitij_987@protonmail.com>
2023-04-02 21:35:39 +0530

    added user controller

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -1,11 +1,36 @@
 import { User } from "../models/userModel.js";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import validator from 'validator'
+import validator from "validator";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //login
 export const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: "Please fill all field" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw Error("Invalid login credentials");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      throw Error("Invalid login credentials");
+    }
+
+    const token = createToken(user._id);
+    res.status(200).json({email, token, message: "logged in sucessfully"});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signup
@@ -13,7 +38,15 @@ export const signupUser = async (req, res) => {
   const { email, password } = req.body;
 
   //VALIDATION
-
+  if (!email || !password) {
+    return res.status(400).json({ error: "Please fill all field" });
+  }
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: "Not an valid email" });
+  }
+  if (!validator.isStrongPassword(password)) {
+    return res.status(400).json({ error: "Password not strong enough" });
+  }
 
   try {
     const exists = await User.findOne({ email });
@@ -25,7 +58,10 @@ export const signupUser = async (req, res) => {
 
     const user = await User.create({ email, password: hash });
 
-    res.status(200).json({ email, user });
+    //create a jwt token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
