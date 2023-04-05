ceef4ce8d7ae521dc1bc9c85b56b0b13a0bd1188
kshitij <kshitij_987@protonmail.com>
2023-04-05 18:41:19 +0530

    added funcationality for signup

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 export default function SignUp() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {signup, isLoading, error} = useSignup()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await signup(email, password)
   }
   return (
     <form className="signup" onSubmit={handleSubmit}>
@@ -24,7 +26,8 @@ export default function SignUp() {
         value={password}
       />
 
-      <button>Sign Up</button>
+      <button disabled={isLoading}>Sign Up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 }
