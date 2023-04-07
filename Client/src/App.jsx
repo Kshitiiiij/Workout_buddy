979eda6df7cc56c4f892568a939bfc15f6eb75b9
kshitij <kshitij_987@protonmail.com>
2023-04-07 23:09:39 +0530

    redirecting logic

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Login from '../src/pages/Login'
 import SignUp from '../src/pages/SignUp'
+import {useAuthContext} from '../src/hooks/useAuthContext'
 
 function App() {
+  const {user} = useAuthContext()
 
   return (
     <div className="App">
@@ -12,9 +14,9 @@ function App() {
       <div className="pages">
        
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
+          <Route path='/' element={user ? <Home />: <Navigate to='/login'/> } />
+          <Route path='/login' element={!user ?<Login />: <Navigate to = '/' />} />
+          <Route path='/signup' element={!user?<SignUp />: <Navigate to='/' />} />
 
         </Routes>
       </div>
